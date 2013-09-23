e0905655db94446c2f3eb0e502c3664311347495
Hartog C. de Mik <hartog@organisedminds.com>
2013-09-23 16:03:20 +0200

    Go into singleton resource mode for user

--- lib/om_api_client/user.rb
+++ lib/om_api_client/user.rb
@@ -1,9 +1,13 @@
 module OM::Api::User
   def me
-    get('/api/users/me')
+    get('/api/user')
   end
 
-  def update_user(id, data)
-    put("/api/users/#{id}", data)
+  def update_user(data)
+    put("/api/user", { user: data })
+  end
+
+  def user_following(type)
+    get('/api/user/following', { type: type })
   end
 end
