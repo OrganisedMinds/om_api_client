5c50b76388b4f8c46479dd75290b8989f0fcc4cb
Hartog C. de Mik <hartog@organisedminds.com>
2013-09-24 12:33:14 +0200

    Following the api

--- lib/om_api_client/user.rb
+++ lib/om_api_client/user.rb
@@ -1,13 +1,26 @@
 module OM::Api::User
   def me
-    get('/api/user')
+    get('/api/me')
+  end
+  alias_method :user, :me
+
+  def update_me(data)
+    put("/api/me", { user: data })
   end
 
-  def update_user(data)
-    put("/api/user", { user: data })
+  def me_follows(type=nil)
+    get('/api/me/follows', { type: type })
   end
 
-  def user_following(type)
-    get('/api/user/following', { type: type })
+  def my_connections
+    get("/api/me/connections")
+  end
+
+  def rate_limit
+    get("/api/me/rate_limit")
+  end
+
+  def users(slug=nil)
+    get("/api/users" + (slug ? "/#{slug}" : ""))
   end
 end
