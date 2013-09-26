cbf13fa839c0b4f799154fd353cb4814daefabe3
Jiri Chara <jirik.chara@gmail.com>
2013-09-26 09:10:55 +0200

    Added workspace update/destroy

--- lib/om_api_client/workspace.rb
+++ lib/om_api_client/workspace.rb
@@ -7,11 +7,25 @@ module OM::Api::Workspace
     get("/api/workspaces/#{id}")
   end
 
-  def create_workspace(owner_slug, name, description, settings={})
+  def create_workspace(opts={})
     post("/api/workspaces", {
-      name:        name,
-      description: description,
-      settings:    settings
+      name:        opts[:name],
+      description: opts[:description],
+      setting:     opts[:setting]
+    })
+  end
+
+  def update_workspace(id, opts={})
+    put("/api/workspaces/#{id}", {
+      name:        opts[:name],
+      description: opts[:description],
+      setting:     opts[:setting]
+    })
+  end
+
+  def destroy_workspace(id, opts={})
+    delete("/api/workspaces/#{id}", {
+      archive_items: opts[:archive_items] || false
     })
   end
 end
