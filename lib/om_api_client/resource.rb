d583c811128a4a01f72a621293b8e73404076423
Hartog C. de Mik <hartog@organisedminds.com>
2013-09-26 14:15:41 +0200

    Switch over from Sawyer to Faraday
    - Sawyer has more then we need

---
+++ lib/om_api_client/resource.rb
@@ -0,0 +1,23 @@
+class OM::Api::Resource < Hash
+  def self.absorb(hash)
+    res = self.new.merge(hash)
+
+    res.each do |k,v|
+      if v.is_a?(Hash)
+        res[k] = absorb(v)
+      elsif v.is_a?(Array)
+        res[k] = v.map { |i| i.is_a?(Hash) ? absorb(i) : i }
+      end
+    end
+  end
+
+  def method_missing(name, *args, &block)
+    super
+  rescue NoMethodError => e
+    name = name.to_s
+
+    raise e unless self.keys.include?(name)
+
+    self[name]
+  end
+end
