e748ec2f91bb2030b25cdde264dfd4a8712bac71
Hartog C. de Mik <hartog@organisedminds.com>
2013-12-03 11:58:17 +0100

    Make workspace specs green

--- spec/workspace_spec.rb
+++ spec/workspace_spec.rb
@@ -2,6 +2,7 @@ require 'rspec_helper'
 
 describe OM::Api::Workspace do
   let(:client) { OM::Api::Client.new( client_id: 1, client_secret: 2 ) }
+  let(:id) { "$id" }
 
   it "should request user's workspaces" do
     client.should_receive(:request).with(:get, "/api/workspaces", {})
@@ -10,7 +11,7 @@ describe OM::Api::Workspace do
 
   it "should request a single workspace" do
     client.should_receive(:request).with(:get, "/api/workspaces/$id", {})
-    client.workspace("$id")
+    client.workspace(id)
   end
 
   it "should request workspace where user is invited" do
@@ -22,10 +23,10 @@ describe OM::Api::Workspace do
     email = Faker::Internet.email
 
     client.should_receive(:request).with(
-      :post, "/api/workspaces/$id/invitation", { email: [ email ] }
+      :post, "/api/workspaces/$id/invitation", { email: [ email ], cc: "false" }
     )
 
-    client.invite_to_workspace("$id", email)
+    client.invite_to_workspace(id, email: email)
   end
 
   it "should request a create of a workspace" do
@@ -35,7 +36,7 @@ describe OM::Api::Workspace do
 
   it "should request a update of a worksapce" do
     client.should_receive(:request).with(:put, "/api/workspaces/$id", {name: "bar"})
-    client.update_workspace("$id", name: "bar")
+    client.update_workspace(id, name: "bar")
   end
 
   it "should request a destroy of a workspace" do
@@ -43,7 +44,7 @@ describe OM::Api::Workspace do
       archive_items: true
     })
 
-    client.destroy_workspace("$id", true)
+    client.destroy_workspace(id, true)
   end
 
   it "should should defaults destroy without archiving" do
@@ -51,16 +52,16 @@ describe OM::Api::Workspace do
       archive_items: false
     })
 
-    client.destroy_workspace("$id")
+    client.destroy_workspace(id)
   end
 
   it "should request a workspace inbox" do
-    client.should_receive(:request).with(:get, "/api/workspaces/$id/inbox")
-    client.workspace_inbox("$id")
+    client.should_receive(:request).with(:get, "/api/workspaces/$id/inbox", {})
+    client.workspace_inbox(id)
   end
 
   it "should create an item on the inbox" do
     client.should_receive(:request).with(:put, "/api/workspaces/$id/inbox", { type: "Activity", id: 12 } )
-    client.workspace_inbox_add(type: "Activity", id: 12)
+    client.workspace_inbox_add(id, type: "Activity", id: 12)
   end
 end
