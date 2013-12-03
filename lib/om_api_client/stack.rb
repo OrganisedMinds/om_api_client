1aeb63afa6a79494afbae1d4300e2dff95e15144
Hartog C. de Mik <hartog@organisedminds.com>
2013-12-03 12:14:04 +0100

    More specs and updated docs

--- lib/om_api_client/stack.rb
+++ lib/om_api_client/stack.rb
@@ -6,7 +6,7 @@ module OM::Api
     #
     # @param id [Integer] The id of the workspace
     # @param opts [Hash] Additional parameters for the query
-    # @return [Array<OM::Api::Resource>] - Stacks
+    # @return [Array<OM::Api::Resource>] Stacks
     #
     def workspace_stacks(id, opts={})
       get("/api/workspaces/#{id}/stacks")
@@ -15,7 +15,7 @@ module OM::Api
     # Get single stack
     #
     # @param id [Integer] The id of the stack
-    # @return [OM::Api::Resource] - The stack
+    # @return [OM::Api::Resource] The stack
     #
     def stack(id)
       get("/api/stacks/#{id}")
@@ -26,7 +26,7 @@ module OM::Api
     # @param opts [Hash] options to create a stack with
     # @option opts [String] :name The name
     # @option opts [String] :description The description
-    # @return [OM::Api::Resource] - Newly created stack
+    # @return [OM::Api::Resource] Newly created stack
     #
     def create_workspace_stack(id, opts={})
       post("/api/workspaces/#{id}/stacks", opts)
@@ -37,7 +37,7 @@ module OM::Api
     # @param opts [Hash] options to update a stack with
     # @option opts [String] :name The name
     # @option opts [String] :description The description
-    # @return [OM::Api::Resource] - Updated stack
+    # @return [OM::Api::Resource] Updated stack
     #
     def update_stack(id, opts={})
       put("/api/stacks/#{id}", opts)
@@ -56,10 +56,27 @@ module OM::Api
     # @param id [Integer] The id of the stack to reorder
     # @param opts [Hash] options to reorder a stack with
     # @option opts [Integer] :position to reorder stack to
-    # @return [OM::Api::Resource] - Updated stack
+    # @return [OM::Api::Resource] Updated stack
     #
-    def reorder_worksapce_stacks(workspace_id, id, opts={})
+    # @note: Does not compute
+    def reorder_workspace_stacks(workspace_id, id, opts={})
       put("/api/workspaces/#{workspace_id}/stacks/#{id}/reorder", opts)
+    end
+
+    # Fetch all items off the stack
+    # @param id [Integer] the id of the stack
+    # @return [Array<OM::Api::Resource>] The stack items
+    def stack_items(id)
+      get("/api/stacks/#{id}/items")
+    end
+
+    # @param id [Integer] The id of the workspace
+    # @param opts [Hash] options to find the item to inbox
+    # @option opts [String] :type Type of item ("Activity" for instance)
+    # @option opts [String] :id Id of the item
+    #
+    def create_stack_item(id, opts)
+      post("/api/stacks/#{id}/items", opts)
     end
   end
 end
